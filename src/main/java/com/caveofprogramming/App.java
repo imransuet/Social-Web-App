e1fb0078bccaa8ea3135513bed2c4a2fb9ec99d8
imransuet <imransuet@gmail.com>
2020-03-20 00:10:14 +0100

    2nd commit

--- src/main/java/com/caveofprogramming/App.java
+++ src/main/java/com/caveofprogramming/App.java
@@ -1,13 +1,15 @@
-package com.example.demo;
+package com.caveofprogramming;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SocialWebAppApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SocialWebAppApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
+	
+	
 
 }
